b6cb8a359088b95e7637c8d6bc10526efb0601bc
Mickael Mignard <mignard.mickael@gmail.com>
2021-05-25 09:14:08 +0200

    Mise au propre du code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-function viderChamps(){
+/**
+ * Clear the fields of the form
+ */
+function clearFields(){
     document.getElementById('lastname').value="";
     document.getElementById('firstname').value=""
     document.getElementById('city').value="";
